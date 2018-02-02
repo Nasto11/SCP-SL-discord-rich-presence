5975fdd3ef7661a9edefa4d93bfce255f65a2147
Nasto11 <kubacz1132@outlook.com>
2018-02-02 18:26:00 +0100

    First Commit

---
+++ Assets/Unixtime.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Unixtime : MonoBehaviour {
+
+	// Use this for initialization
+	public void tak () 
+	{
+		 System.DateTime epochStart = new System.DateTime(1970, 1, 1, 0, 0, 0, System.DateTimeKind.Utc);
+ 		 long cur_time = (long)(System.DateTime.UtcNow - epochStart).TotalSeconds;
+
+	}
+	
+}
